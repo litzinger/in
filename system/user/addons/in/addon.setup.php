c90f5a5168c1f60674179a4156bd7190978b7135
Brian Litzinger <brian.litzinger@jamfsoftware.com>
2017-05-25 09:43:31 -0500

    Convert tabs to spaces, as it should be.

--- system/user/addons/in/addon.setup.php
+++ system/user/addons/in/addon.setup.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined('IN_VERSION')) {
-	define('IN_VERSION', '1.4');
+    define('IN_VERSION', '1.4');
 }
 
 return [
--- system/user/addons/in/ext.in.php
+++ system/user/addons/in/ext.in.php
@@ -16,85 +16,85 @@ class In_ext
     /**
      * @var array
      */
-	public $settings = [];
+    public $settings = [];
 
     /**
      * @var string
      */
-	public $version = IN_VERSION;
+    public $version = IN_VERSION;
 
     /**
      * @param array $settings
      */
     public function __construct($settings = [])
-	{
-		$this->settings = $settings;
-	}
+    {
+        $this->settings = $settings;
+    }
 
-	/**
-	 * Activate the extension by entering it into the exp_extensions table
-	 *
-	 * @return void
-	 */
-	public function activate_extension()
-	{
-		$hooks = [
-			'template_fetch_template' => 'template_fetch_template'
+    /**
+     * Activate the extension by entering it into the exp_extensions table
+     *
+     * @return void
+     */
+    public function activate_extension()
+    {
+        $hooks = [
+            'template_fetch_template' => 'template_fetch_template'
         ];
 
-		foreach ($hooks as $hook => $method) {
-			$data = [
-				'class'		=> __CLASS__,
-				'method'	=> $method,
-				'hook'		=> $hook,
-				'settings'	=> serialize([]),
-				'priority'	=> 9,
-				'version'	=> IN_VERSION,
-				'enabled'	=> 'y'
+        foreach ($hooks as $hook => $method) {
+            $data = [
+                'class'     => __CLASS__,
+                'method'    => $method,
+                'hook'      => $hook,
+                'settings'  => serialize([]),
+                'priority'  => 9,
+                'version'   => IN_VERSION,
+                'enabled'   => 'y'
             ];
 
-			ee()->db->insert('extensions', $data);
-		}
-	}
+            ee()->db->insert('extensions', $data);
+        }
+    }
 
-	/**
-	 * Disables the extension by removing it from the exp_extensions table.
-	 *
-	 * @return void
-	 */
-	function disable_extension()
-	{
-		ee()->db->where('class', __CLASS__);
-		ee()->db->delete('extensions');
-	}
+    /**
+     * Disables the extension by removing it from the exp_extensions table.
+     *
+     * @return void
+     */
+    function disable_extension()
+    {
+        ee()->db->where('class', __CLASS__);
+        ee()->db->delete('extensions');
+    }
 
-	/**
-	 * Updates the extension by performing any necessary db updates when the extension page is visited.
-	 *
-	 * @param string $current
-	 * @return mixed void on update, false if none
-	 */
-	function update_extension($current = '')
-	{
-		if ($current == '' OR $current == IN_VERSION) {
-			return false;
-		}
+    /**
+     * Updates the extension by performing any necessary db updates when the extension page is visited.
+     *
+     * @param string $current
+     * @return mixed void on update, false if none
+     */
+    function update_extension($current = '')
+    {
+        if ($current == '' OR $current == IN_VERSION) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Creates global variables from the template pre parse data.
-	 *
-	 * @param $row
-	 * @return string
-	 */
-	public function template_fetch_template($row)
-	{
+    /**
+     * Creates global variables from the template pre parse data.
+     *
+     * @param $row
+     * @return string
+     */
+    public function template_fetch_template($row)
+    {
         $row['template_data'] = $this->parse_inserts($row['template_data']);
 
         return $row;
-	}
+    }
 
     /**
      * Old installations were installed with template_pre_parse as the method name.
@@ -104,7 +104,7 @@ class In_ext
      *
      * @deprecated
      */
-	public function template_pre_parse($row) {
+    public function template_pre_parse($row) {
         return $this->template_fetch_template($row);
     }
 
